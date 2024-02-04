f63342b851305c463015cba44684346d57e65053
abdallahowais <abdallah.owis@students.alquds.edu>
2024-02-04 17:38:45 +0200

    abdate and do the right code

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const sidbar = document.querySelector('.sidbar');
 const back = document.querySelector('.sidbar span');
 const show = document.querySelector('h2');
+show.onclick = () => {
+    show.innerHTML=("This is a sidbar")
+    show.style.zIndex=99999999999;
+    sidbar.classList.add('d-block');
+    document.querySelector(".overlay").classList.add('d-block')
+    document.querySelector(".overlay").style()
+};
+
 
 back.onclick = () => {
-    sidbar.classList.add('hide'); // إضافة الكلاس "hide" للشريط الجانبي
-    document.querySelector(".overlay").classList.add('d-none')
-
-};
-
-show.onclick = () => {
-    sidbar.classList.remove('hide'); // إضافة الكلاس "hide" للشريط الجانبي
-    document.querySelector(".overlay").classList.remove('d-none')
+    show.innerHTML=("Show sidbar")
+    sidbar.classList.remove('d-block');
+    document.querySelector(".overlay").classList.remove('d-block')
 
 
 };
+
